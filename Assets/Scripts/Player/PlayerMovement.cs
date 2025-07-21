3506a863b012ff0be1eec139f330e9dd0e7e287a
jibbyie <jibbyie@gmail.com>
2025-07-21 20:45:35 +0100

    Imported Live2D SDK, using live2d model from artist with simple idle and walking animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float playerSpeed = 5f;
     [SerializeField] private Rigidbody2D playerRB;
 
+    //Add a reference for the child model
+    [SerializeField] private Transform characterModel;
+
+    private Animator animator;
+
     private void Awake()
     {
-        if(playerRB == null)
+        if (playerRB == null)
         {
             playerRB = GetComponent<Rigidbody2D>();
         }
+        // Get the Animator from the child object
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void FixedUpdate()
@@ -23,6 +30,21 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        playerRB.linearVelocity = new Vector2(horizontalInput *  playerSpeed, verticalInput * playerSpeed);
+        playerRB.linearVelocity = new Vector2(horizontalInput * playerSpeed, verticalInput * playerSpeed);
+
+        bool isMoving = playerRB.linearVelocity.magnitude > 0.1f;
+        animator.SetBool("isWalking", isMoving);
+
+        // Flip the child model, not the parent 
+        if (horizontalInput > 0.01f)
+        {
+            // Facing Right
+            characterModel.localScale = new Vector3(Mathf.Abs(characterModel.localScale.x), characterModel.localScale.y, characterModel.localScale.z);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            // Facing Left
+            characterModel.localScale = new Vector3(-Mathf.Abs(characterModel.localScale.x), characterModel.localScale.y, characterModel.localScale.z);
+        }
     }
 }
